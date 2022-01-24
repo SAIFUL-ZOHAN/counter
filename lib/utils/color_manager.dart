b8dd54130b2257e77bc7dcf5ee8024976159da3f
SAIFUL-ZOHAN.com <shagor636462@gmail.com>
2022-01-24 10:41:32 +0600

    counter

---
+++ lib/utils/color_manager.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color PRIMARY = Colors.white;
+  static Color SECONDARY = Colors.purple;
+  static Color ACCENT = Colors.black;
+
+  static const List<Color> colors = [
+    Colors.green,
+    Colors.cyan,
+    Colors.blue,
+    Colors.indigo,
+    Colors.orange,
+    Colors.purple,
+    Colors.pink,
+  ];
+}
